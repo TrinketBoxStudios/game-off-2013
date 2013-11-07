c8dfd0876faba2127e2a4b02cee1beae424d81ed
ryderhanson <ryder.hanson@gmail.com>
2013-11-06 23:22:42 -0800

    Commiting initial player and platform prefabs and scripts

---
+++ Change/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public GameObject followTarget;
+	
+	private Vector3 _currentPositon;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		_currentPositon = followTarget.transform.position;
+		
+		// let the camera keep its z position
+		_currentPositon.z = transform.position.z;
+		
+		transform.position = _currentPositon;
+	}
+}
---
+++ Change/Assets/Scripts/ConstantMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantMovement : MonoBehaviour 
+{
+	public float movementSpeed;
+	public float jumpHeight;
+	
+	private Vector3 _playerVelocity;
+	
+	private Rigidbody _rigidbody;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		_rigidbody = rigidbody;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		_playerVelocity = _rigidbody.velocity;
+		
+		if(Input.GetKeyDown(KeyCode.Space) )
+		{
+			_playerVelocity.y += jumpHeight;	
+		}
+		
+		_playerVelocity.x = movementSpeed;
+		
+		_rigidbody.velocity = _playerVelocity;
+	}
+}
