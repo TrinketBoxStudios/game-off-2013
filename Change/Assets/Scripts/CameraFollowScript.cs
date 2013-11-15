c46711058ee179915f3e9c6c903b144aff97921d
ryderhanson <ryder.hanson@gmail.com>
2013-11-14 22:48:28 -0800

    Upgrading to Unity 4.3 and modifying a few project structure elements

---
+++ Change/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+
+	public GameObject followTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 newPos = followTarget.transform.position;
+
+		newPos.z = transform.position.z;
+
+		transform.position = newPos;
+	}
+}
